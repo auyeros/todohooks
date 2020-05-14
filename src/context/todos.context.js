e2316f81820a13137cd6570efe4864ac2715964b
Ignacio <54366484+auyeros@users.noreply.github.com>
2020-05-14 14:40:43 -0300

    Split into 2 contexts

--- src/context/todos.context.js
+++ src/context/todos.context.js
@@ -1,16 +1,21 @@
 import React, { createContext, useReducer } from "react";
 import todoReducer from "../reducers/todo.reducer";
-import useTodosState from "../Hooks/useTodosState";
+
 const defaultTodos = [
     { id: 1, task: "Break the Dishes", completed: false },
     { id: 2, task: "Give birth to the AntiChrist", completed: false },
 ];
 export const TodosContext = createContext();
+export const DispatchContext = createContext();
+
 export function TodosProvider(props) {
     const [todos, dispatch] = useReducer(todoReducer, defaultTodos);
     return (
-        <TodosContext.Provider value={{ todos, dispatch }}>
-            {props.children}
+        //Prevents RErendering
+        <TodosContext.Provider value={todos}>
+            <DispatchContext.Provider value={dispatch}>
+                {props.children}
+            </DispatchContext.Provider>
         </TodosContext.Provider>
     );
 }
